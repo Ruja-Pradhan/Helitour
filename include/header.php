a757965d9a9a117295127925a87d8dda7bf0e609
Ruja pradhan <60001747+Ruja-Pradhan@users.noreply.github.com>
2021-11-19 07:56:18 +0545

    Project done by ruja
    This is the website developed to promote tourism sector of Nepal.

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<head>
+    <title>HeliTour</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js" ></script>
+   
+    
+</head>
+<body>
+ 
+          <a class="navbar-brand" href="index.php"><font face="papyrus">HeliTour</font></a>
---
+++ tour_packages.php
@@ -0,0 +1,5 @@
+<div class="container-fluid">
+<?php include_once('include/header.php'); ?>
+<?php include_once('include/slider.php'); ?>
+<?php include_once('include/footer.php'); ?>
+</div>
